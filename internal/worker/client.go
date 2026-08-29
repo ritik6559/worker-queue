d3d6b5ab88d5b437261d77ddbea9c0b6f96e6bc6
ritik6559 <ritikjoshi741@gmail.com>
2026-08-29 16:41:17 +0530

    feat: implement Ack and Nack methods for task acknowledgment and error reporting

--- internal/worker/client.go
+++ internal/worker/client.go
@@ -63,6 +63,47 @@ func (c *Client) Next(ctx context.Context, maxWait, holdFor time.Duration) (*sto
 	}
 }
 
+func (c *Client) Ack(ctx context.Context, taskID, leaseID string) error {
+	return c.report(ctx, taskID, leaseID, "ack", nil)
+}
+
+type nackBody struct {
+	Error string `json:"error"`
+}
+
+func (c *Client) Nack(ctx context.Context, taskID, leaseID, reason string) error {
+	body, err := json.Marshal(nackBody{Error: reason})
+	if err != nil {
+		return err
+	}
+
+	return c.report(ctx, taskID, leaseID, "nack", body)
+}
+
+func (c *Client) report(ctx context.Context, taskID, leaseID, verb string, body []byte) error {
+	address := fmt.Sprintf("%s/jobs/%s/%s?lease_id=%s", c.brokerURL, url.PathEscape(taskID), verb, url.QueryEscape(leaseID))
+
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, address, bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+	if body != nil {
+		request.Header.Set("Content-Type", "application/json")
+	}
+
+	response, err := c.http.Do(request)
+	if err != nil {
+		return err
+	}
+	defer closeBody(response)
+
+	if response.StatusCode != http.StatusNoContent {
+		return unexpectedStatus(response)
+	}
+
+	return nil
+}
+
 func unexpectedStatus(response *http.Response) error {
 	detail, _ := io.ReadAll(io.LimitReader(response.Body, 512))
 	return fmt.Errorf("broker returned %s: %s", response.Status, bytes.TrimSpace(detail))
